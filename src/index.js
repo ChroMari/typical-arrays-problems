8c0201f8922ca226ce3668fafd1d3e7564fb7aeb
ChroMari <chromarija@gmail.com>
2021-02-08 16:36:59 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = min = (array) => (array == undefined || array.length == 0) ? 0 : Math.min(...array);
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = max = (array) => (array == undefined || array.length == 0) ? 0 : Math.max(...array);
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined || array.length == 0) return 0;
+  let len = array.length;
+  return array.reduce((sum, item) => sum += item, 0)/len;
 }
